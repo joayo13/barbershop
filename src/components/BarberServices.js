614bfa54fbf42278527254a30f7d871563832216
Jordan Ayotte <jordanayotte13@outlook.com>
2023-10-06 12:20:18 -0400

    started changng services section to use grid

--- src/components/BarberServices.js
+++ src/components/BarberServices.js
@@ -1,87 +1,79 @@
 function BarberServices() {
     return (
       <section>
-        <h2 className="text-center text-3xl text-red-800">Barber Services</h2>
-        <ul className="max-w-lg grid grid-cols-2 grid-rows-2 text-center mx-auto">
-        <div>
-        <h2>Signature Cut & Style</h2>
+        <h2 className="text-center text-3xl py-4 text-red-800">Barber Services</h2>
+        <ul className="max-w-6xl grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 text-center mx-auto">
+        <div className="flex items-end">
+        <h2 className="font-bold min-w-fit">Signature Cut & Style</h2>
+        <div className="h-px w-full mb-1 bg-neutral-400"></div>
         <p>$45.00+</p>
-        <p>First visit or has it been longer than 5 weeks? Please book this service!</p>
         </div>
         <div>
-        <h2>Maintenance Cut</h2>
+        <h2 className="font-bold">Maintenance Cut</h2>
         <p>$35.00+</p>
-        <p>30 min service with just the basics. For those visiting every 5 weeks or less.</p>
         </div>
         <div>
-        <h2>Barber Cut - 13 & Under (Formerly Boys Cut)</h2>
+        <h2 className="font-bold">Barber Cut - 13 & Under</h2>
         <p>$31.00+</p>
-        <p>Barber style haircuts (clipper and short scissor cuts).</p>
         </div>
         <div>
-        <h2>Advanced Fade/Design</h2>
+        <h2 className="font-bold">Advanced Fade/Design</h2>
         <p>$65.00+</p>
-        <p>$1/min</p>
         </div>
         <div>
-        <h2>Straight Blade Head Shave</h2>
+        <h2 className="font-bold">Straight Blade Head Shave</h2>
         <p>$45.00+</p>
         </div>
         <div>
-        <h2>Neck Clean Up</h2>
-        <p>$22.00+</p>
-        <p>Clean up perimeter of hair and shave off back of the neck.</p>
-        </div>
-        <div>
-        <h2>Beard Trim</h2>
+        <h2 className="font-bold">Neck Clean Up</h2>
         <p>$22.00+</p>
         </div>
         <div>
-        <h2>Beard Shaping with Face Shape Analysis</h2>
+        <h2 className="font-bold">Beard Trim</h2>
+        <p>$22.00+</p>
+        </div>
+        <div>
+        <h2 className="font-bold">Beard Shaping with Face Shape Analysis</h2>
         <p>$25.00+</p>
         </div>
         <div>
-        <h2>Beard Cleanse & Condition</h2>
+        <h2 className="font-bold">Beard Cleanse & Condition</h2>
         <p>$28.00+</p>
         </div>
         <div>
-        <h2>Quick Shave</h2>
+        <h2 className="font-bold">Quick Shave</h2>
         <p>$30.00+</p>
-        <p>Straight Blade shave on cheeks and lower neck area.</p>
         </div>
         <div>
-        <h2>Hot Lather Face Shave</h2>
+        <h2 className="font-bold">Hot Lather Face Shave</h2>
         <p>$71.00+</p>
         </div>
         <div>
-        <h2>Hot Lather Face Shave (With Haircut)</h2>
+        <h2 className="font-bold">Hot Lather Face Shave (With Haircut)</h2>
         <p>$61.00+</p>
-        <p>To be booked in conjunction with haircut service.</p>
         </div>
         <div>
-        <h2>Express Grey Blend (Camo Colour)</h2>
+        <h2 className="font-bold">Express Grey Blend (Camo Colour)</h2>
         <p>$35.00+</p>
         </div>
         <div>
-        <h2>Colour</h2>
+        <h2 className="font-bold">Colour</h2>
         <p>$85.00+</p>
-        <p>Permanent Colour - Does not include grey blending colours.</p>
         </div>
         <div>
-        <h2>Corrective Colour - Barber</h2>
+        <h2 className="font-bold">Corrective Colour - Barber</h2>
         <p>$128.00+</p>
-        <p>Per hour, based on consultation.</p>
         </div>
         <div>
-        <h2>Beard Colour</h2>
+        <h2 className="font-bold">Beard Colour</h2>
         <p>$35.00+</p>
         </div>
         <div>
-        <h2>Toner/Additional Colours</h2>
+        <h2 className="font-bold">Toner/Additional Colours</h2>
         <p>$32.00+</p>
         </div>
         <div>
-        <h2>Scalp Treatment</h2>
+        <h2 className="font-bold">Scalp Treatment</h2>
         <p>$22.00+</p>
         </div>
         </ul>
